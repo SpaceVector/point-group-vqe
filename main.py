0a7f84e1f11b15284f4b0b8095038f5d00c0d4ee
SpaceVector <919588524@qq.com>
2023-03-29 10:40:58 +0000

    更新运行代码

--- main.py
+++ main.py
@@ -2,21 +2,25 @@ from openfermion.chem import MolecularData
 from openfermionpyscf import run_pyscf
 from mindquantum.core.gates import X
 from mindquantum.core.circuit import Circuit
-from mindquantum.core.operators import Hamiltonian
+from mindquantum.core.operators import Hamiltonian, TimeEvolution
 from mindquantum.simulator import Simulator
-from mindquantum.algorithm.nisq import generate_uccsd
+from mindquantum.algorithm.nisq import Transform, get_qubit_hamiltonian
 import mindspore as ms
 from mindquantum.framework import MQAnsatzOnlyLayer
-import matplotlib.pyplot as plt
-import numpy as np
 from timeit import default_timer
+from pyscf import gto, scf, symm
+from SymUCCSD import uccsd_singlet_generator
 
 
 ms.set_context(mode=ms.PYNATIVE_MODE, device_target="CPU")
 geometry = [
-    ["O", [0.0, 0.0, 0.137]],
-    ["H", [0.0, 0.769, -0.546]],
-    ["H", [0.0, -0.769, -0.546]]
+    # ["Be", [0.0, 0.0, 0.0]],
+    # ["H", [0.0, 0.0, 1.291]],
+    # ["H", [0.0, 0.0, -1.291]]
+    # ["Li",[0.0, 0.0, 0.378]],
+    # ["H", [0.0, 0.0, -1.133]]
+    ["F",[0.0, 0.0, 0.096]],
+    ["H", [0.0, 0.0, -0.860]]
 ]
 basis = "sto3g"
 spin = 0
@@ -27,14 +31,34 @@ molecule_of = MolecularData(
     multiplicity=2 * spin + 1
 )
 molecule_of = run_pyscf(
-    molecule_of,
-    run_fci=1,
-    run_ccsd=1
+    molecule_of
 )
+mol = gto.Mole()
+mol.atom = molecule_of.geometry
+mol.basis = molecule_of.basis
+mol.spin = molecule_of.multiplicity - 1
+mol.charge = molecule_of.charge
+mol.symmetry = "C2v"
+mol.build()
+mf = scf.RHF(mol)
+mf.kernel()
+mo_occ = mf.mo_occ #轨道表示数组
+irrep_id = symm.label_orb_symm(mol, mol.irrep_id, mol.symm_orb, mf.mo_coeff)
+# 被占据轨道数
+n_occupied = mol.nelec[0]
+# 总轨道数
+n_virtual = mf.mo_coeff.shape[1]-n_occupied
 
+ucc_fermion_ops = uccsd_singlet_generator(n_virtual, n_occupied, irrep_id, mo_occ, anti_hermitian=True)
+
+hamiltonian_QubitOp = get_qubit_hamiltonian(molecule_of)
+
+ucc_qubit_ops = Transform(ucc_fermion_ops).jordan_wigner()
+ansatz_circuit = TimeEvolution(ucc_qubit_ops.imag, 1.0).circuit
+ansatz_parameter_names = ansatz_circuit.params_name
 hartreefock_wfn_circuit = Circuit([X.on(i) for i in range(molecule_of.n_electrons)])
-ansatz_circuit, init_amplitudes, ansatz_parameter_names, hamiltonian_QubitOp, n_qubits, n_electrons = generate_uccsd(molecule_of, threshold=-1)
 total_circuit = hartreefock_wfn_circuit + ansatz_circuit
+total_circuit.summary()
 
 start_time = default_timer()    # 程序开始时间
 sim = Simulator('mqvector', total_circuit.n_qubits)
