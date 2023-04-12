c0e06a1444435cc629b16fe9cff415c9f38c67dc
SpaceVector <919588524@qq.com>
2023-04-12 09:53:05 +0000

    更新SCIPY代码

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from mindquantum.core.circuit import Circuit
 from mindquantum.core.operators import Hamiltonian, TimeEvolution
 from mindquantum.simulator import Simulator
 from mindquantum.algorithm.nisq import Transform, get_qubit_hamiltonian
-import mindspore as ms
-from mindquantum.framework import MQAnsatzOnlyLayer
 from timeit import default_timer
 from pyscf import gto, scf, symm
 from SymUCCSD import uccsd_singlet_generator
+import mindquantum
+from scipy.optimize import minimize
+import numpy as np
 
 
-ms.set_context(mode=ms.PYNATIVE_MODE, device_target="CPU")
 geometry = [
-    ["C", [0.0, 0.0, 0.653]],
-    ["C", [0.0, 0.0, -0.653]],
-    ["H", [0.0, 0.916, 1.229]],
-    ["H", [0.0, -0.916, 1.229]],
-    ["H", [0.0, -0.916, -1.229]],
-    ["H", [0.0, 0.916, -1.229]]
+    # ["C", [0.0, 0.0, 0.653]],
+    # ["C", [0.0, 0.0, -0.653]],
+    # ["H", [0.0, 0.916, 1.229]],
+    # ["H", [0.0, -0.916, 1.229]],
+    # ["H", [0.0, -0.916, -1.229]],
+    # ["H", [0.0, 0.916, -1.229]]
     # ["O", [0.0, 0.0, 0.127]],
     # ["H", [0.0, 0.758, -0.509]],
     # ["H", [0.0, -0.758, -0.509]]
@@ -30,6 +30,11 @@ geometry = [
     # ["H", [0.0, 0.0, -1.133]]
     # ["F",[0.0, 0.0, 0.096]],
     # ["H", [0.0, 0.0, -0.860]]
+    ["C", [0.0, 0.0, 0.0]],
+    ["H", [0.625, 0.625, 0.625]],
+    ["H", [-0.625, -0.625, 0.625]],
+    ["H", [-0.625, 0.625, -0.625]],
+    ["H", [0.625, -0.625, -0.625]]
 ]
 basis = "sto3g"
 spin = 0
@@ -48,7 +53,7 @@ mol.atom = molecule_of.geometry
 mol.basis = molecule_of.basis
 mol.spin = molecule_of.multiplicity - 1
 mol.charge = molecule_of.charge
-mol.symmetry = "D2h"
+mol.symmetry = "D2"
 mol.build()
 mf = scf.RHF(mol)
 mf.kernel()
@@ -73,30 +78,27 @@ total_circuit.summary()
 start_time = default_timer()    # 程序开始时间
 sim = Simulator('mqvector', total_circuit.n_qubits)
 molecule_pqc = sim.get_expectation_with_grad(Hamiltonian(hamiltonian_QubitOp), total_circuit)
-molecule_pqcnet = MQAnsatzOnlyLayer(molecule_pqc, 'Zeros')
-initial_energy = molecule_pqcnet()
 
-optimizer = ms.nn.Adagrad(molecule_pqcnet.trainable_params(), learning_rate=4e-2)
-train_pqcnet = ms.nn.TrainOneStepCell(molecule_pqcnet, optimizer)
-
-eps = 1.e-8
-energy_diff = eps * 1000
-energy_last = initial_energy.asnumpy() + energy_diff
-
-iter_idx = 0
-
-while abs(energy_diff) > eps:
-    energy_i = train_pqcnet().asnumpy()
-    # if iter_idx % 5 == 0:
-    #     print("Step %3d energy %20.16f" % (iter_idx, float(energy_i)))
-    energy_diff = energy_last - energy_i
-    energy_last = energy_i
+iter_idx = 1
+def func(x):
+    n,g = molecule_pqc(x)
+    global iter_idx
+    if iter_idx % 3 == 0:
+        end_time = default_timer()    # 程序结束时间
+        run_time = end_time - start_time    # 程序的运行时间，单位为秒
+        print('第',iter_idx,'次迭代，当前能量为：', np.real(np.squeeze(n)),'当前用时：',run_time)
     iter_idx += 1
+    return np.real(np.squeeze(n)), np.real(np.squeeze(g))
+
+
+init_amp = [0.0]*len(ansatz_parameter_names)
+res = minimize(func, init_amp, jac=True, method='TNC', tol=0.0015)
+
 end_time = default_timer()    # 程序结束时间
 run_time = end_time - start_time    # 程序的运行时间，单位为秒
 print("Molecule: ", molecule_of.name)
 print("Number of parameters: %d" % (len(ansatz_parameter_names)))
 print("Optimization completed at step %3d" % (iter_idx - 1))
-print("Optimized energy: %20.16f" % (energy_i))
+print("Optimized energy: %20.16f" % (res.fun))
 print("Runtime: ", run_time,'s')
 
